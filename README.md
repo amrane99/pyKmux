10f0da771b9ed631b05eff46ec09332275d18e1f
Amin Ranem <aminranem@googlemail.com>
2021-09-14 02:24:18 +0200

    Changed the blank use of password prompt and password within the script by encrypting everything and storing it appropriately.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # pyKmux
 [![License](https://img.shields.io/badge/License-MIT-brightgreen.svg)](LICENSE)
 
-A small and simple python script to automatically renew a kerberos ticket in a tmux session
+A small and simple python script to automatically renew a kerberos ticket in a tmux session. The kinit password prompt and password are encrypted and individually stored as environment variables, whereas the encryption key is stored seperately in a file, at a path specified by the user. The passwords and path need to be specified whenever they are not available during runtime of the script, ie. whenever the file_path eg. is not stored as an environment variable anymore, the user needs to specify the path again. The same applies for the kinit password prompt or password. After sucessful ticket renewal, a new key will be generated, the password prompt and password encrypted using the new key and stored accordingly.
 
 ## Installation
-* Install pexpect with: `pip install pexpect`
-* Clone the pyKmux repo
-* Execute `kinit` in your terminal and copy the entire password prompt (including the colon) into `password_prompt` in `pykmux.py`
-* Type in your password in `password` in `pykmux.py`
+* Clone the pyKmux repository
+* Navigate to the project root (where `pykmux.py` lives)
+* Execute `pip install -r requirements.txt` to install all required packages
+* Execute `kinit` in your terminal and copy the entire password prompt (including the colon)
 
 ## How to use
 * Create or attach to a tmux session: `tmux new` or `tmux a -t X`
 * Open a new tmux pane: `Ctrl+B "`
 * Activate the new pane: `Ctrl+B DOWN`
 * Execute `pykmux.py` every 5 hours: `watch -n 18000 "python pykmux.py"`
+* If prompted: Enter path to folder in which the encryption key is stored as well as password prompt (visible when typing) and password (not visible when typing)
 * (Optional) Hide the pane
--- pykmux.py
+++ pykmux.py
@@ -1,9 +1,91 @@
-import pexpect
+# -- Import necessary modules -- #
+import os, sys, pexpect
+from getpass import getpass
+from cryptography.fernet import Fernet
 
-password_prompt = ''
-password = ''
+def _generate_key():
+    # -- Generate a new encryption key -- #
+    return Fernet.generate_key()
 
-child = pexpect.spawn('kinit')
-child.expect(password_prompt)
-child.sendline(password)
-child.send('\n')
+def _encrypt(key, token):
+    f = Fernet(key)
+    return f.encrypt(token)
+
+def _decrypt(key, token):
+    f = Fernet(key)
+    try:
+        return f.decrypt(token)
+    except (Fernet.InvalidToken, TypeError):  # Catch any InvalidToken exceptions if the correct key was not provided
+        print("Invalid Key - Unsuccessfully decrypted or token not in bytes")
+        print("Exit the script")
+        sys.exit(1)
+
+def _load(file_path):
+    # -- Load the key -- #
+    with open(file_path, 'rb') as key_file:
+        key = key_file.read()
+
+    return key, os.environ['KINIT_PWD_PROMPT'], os.environ['CONNECTION_PWD']
+
+def _store(key, file_path, prompt, pwd):
+    # -- Store them as os.environ -- #
+    os.environ['KINIT_PWD_PROMPT'] = prompt
+    os.environ['CONNECTION_PWD'] = pwd
+    # -- Store key as file at file_path -- #
+    with open(file_path, 'wb') as key_file:
+        key_file.write(key)
+
+def renew_ticket():
+    # -- Extract key encryption key -- #
+    if os.environ['KEYS_PATH'] is None:
+        # -- Ask user for the path -- #
+        print("Please enter the path where the file with the encryption key will be stored: ")
+        file_path = input()
+        # -- Store the path as variable -- #
+        os.environ['KEYS_PATH'] = os.path.join(file_path, '.secret.key')
+        print("The file with the encryption key can be found at: {}.".format(os.environ['KEYS_PATH']))
+    else:
+        file_path = os.environ['KEYS_PATH']
+
+    # -- Extract kinit password prompt and password to connect -- #
+    if os.environ['KINIT_PWD_PROMPT'] is None or os.environ['CONNECTION_PWD'] is None:
+        # -- Generate a new encryption key -- #
+        key = _generate_key()
+        # -- Get the kinit password prompt from the user if it is not already done once and stored in os.environ -- #
+        print("Please enter the password prompt from kinit: ")
+        password_prompt_decrypted = input()
+        # -- Get the password as well -- #
+        password_decrypted = getpass()
+        # -- If the password prompt and password are not stored in the os.environ, then encrypt and store -- #
+        # -- them so we do not need to ask again -- #
+        # -- Encode turns the string to bytes -- #
+        password_prompt_encrypted = _encrypt(key, password_prompt_decrypted.encode())
+        password_encrypted = _encrypt(key, password_decrypted.encode())
+        # -- Store everything -- #
+        _store(key, file_path, password_prompt_encrypted, password_encrypted)
+    else:
+        # -- Load key and prompt with password -- #
+        key, password_prompt, password = _load(file_path)
+        f = Fernet(key)
+        # NOTE: Decode the bytes back to string or the passwords do not match
+        # -- Get kinit password prompt and decrypt it -- #
+        password_prompt_decrypted = _decrypt(key, password_prompt).decode()
+        # -- Get password and decrypt it -- #
+        password_decrypted = _decrypt(key, password).decode()
+        
+
+    # -- Now we can renew the kerberos ticket in a tmux session (the safe encrypted way) -- #
+    child = pexpect.spawn('kinit')
+    child.expect(password_prompt_decrypted)
+    child.sendline(password_decrypted)
+    child.send('\n')
+
+    # -- Encrypt the password prompt and password with a new key and restore everything -- #
+    # -- Generate a new encryption key -- #
+    key = _generate_key()
+    # -- Store everything -- #
+    _store(key, file_path, _encrypt(key, password_prompt_decrypted.encode()), _encrypt(key, password_decrypted.encode()))
+
+
+if __name__ == "__main__":
+    renew_ticket()
